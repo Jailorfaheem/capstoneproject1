6f52a587687dd557254bf4d5ac38acd749780e46
Jailorfaheem <88473629+Jailorfaheem@users.noreply.github.com>
2024-06-20 19:55:58 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,2 @@
 #!/bin/bash
-IMAGE_NAME="capstoneproject"
-DOCKERFILE_PATH="Dockerfile"
-docker build -t "$IMAGE_NAME" -f "DOCKERFILE_PATH" .
-if [$? -eq 0 ]; then
-  echo "Docker image '$IMAGE_NAME' Build Successfully."
-else
- echo "Error: Docker image Build Failed"
-fi
+docker build -t test .
